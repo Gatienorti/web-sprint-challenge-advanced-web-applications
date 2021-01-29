dac8489ba6391e14f7cc098bdf561562378e67b3
Gatien Orti-vinas <gatien.orti@gmail.com>
2021-01-29 13:01:38 -0500

    finish

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import axios from "axios";
+import EditMenu from './EditMenu'
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const initialColor = {
   color: "",
@@ -9,7 +11,7 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
-
+  const history = useHistory()
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
@@ -17,10 +19,27 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-
+    axiosWithAuth()
+      .put(`colors/${colorToEdit.id}`,colorToEdit)
+      .then(res=>{
+        updateColors([...colors, res.data])
+        history.pushState('/bubbles/reload')
+      })
+      .catch(err =>{
+        console.error(err)
+      })
   };
 
   const deleteColor = color => {
+      axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(res =>{
+        console.log(res)
+        history.push('/bubbles/reload')
+      })
+      .catch(err =>{
+        console.error(err)
+      })
   };
 
   return (
